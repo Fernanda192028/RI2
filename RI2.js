9fea56f7f0bae95fbc033338b7fe289fd345742e
Fernanda <fernandafcbpereira@gmail.com>
2026-03-04 19:03:13 -0300

    adiciona pdf e alterações ao código

--- RI2.js
+++ RI2.js
@@ -41,18 +41,17 @@ function Endereco(rua, numero,cidade,estado){
 
 
 
-const telefoneCelular1 = new TelefoneCelular("12", "12981088049");
-const endereco1 = new Endereco("MG", "Minas Gerais", "av.ouro preto", "123");
+const telefoneCelular1 = new TelefoneCelular("31", "12981088049");
+const endereco1 = new Endereco("MG", "Minas Gerais", "av.ouro preto", "027");
 const cliente1 = new Cliente("Noah Conrado Heinz", telefoneCelular1 , "noah.heinz@gmail.com", endereco1);
 
-const telefoneCelular2 = new TelefoneCelular("12", "12981088049");
-const endereco2 = new Endereco("MG", "Minas Gerais", "av.ouro preto", "123");
-const cliente2 = new Cliente("Beatriz Conrado Heinz", telefoneCelular1 , "noah.heinz@gmail.com", endereco1);
+const telefoneCelular2 = new TelefoneCelular("92", "12981077080");
+const endereco2 = new Endereco("AM", "Manaus", "av.Eduardo Ribeiro", "098");
+const cliente2 = new Cliente("Maria Eduarda Assis", telefoneCelular1 , "maria.assis@gmail.com", endereco1);
 
-const telefoneCelular3 = new TelefoneCelular("12", "12981088049");
-const endereco3 = new Endereco("MG", "Minas Gerais", "av.ouro preto", "123");
-const cliente3 = new Cliente("Ana Conrado Heinz", telefoneCelular1 , "noah.heinz@gmail.com", endereco1);
-
+const telefoneCelular3 = new TelefoneCelular("12", "11981099030");
+const endereco3 = new Endereco("SP", "São Paulo", "av.Paulista", "123");
+const cliente3 = new Cliente("Ana Beatriz Costa", telefoneCelular1 , "beatriz.costa@gmail.com", endereco1);
 
 
 
@@ -66,17 +65,41 @@ console.log("Endereço: \n" + endereco1.descricaoEndereco)
 
 console.log("---------------")
 
+
+console.log("\n---------------")
+console.log("Informações do cliente: \n" + cliente2.descricaoCliente)
+console.log("---------------")
+console.log("---------------")
+console.log("Telefone: \n" + telefoneCelular2.descricaoTelefoneCelular)
+console.log("---------------")
+console.log("Endereço: \n" + endereco2.descricaoEndereco)
+
+console.log("---------------")
+
+
+console.log("\n---------------")
+console.log("Informações do cliente: \n" + cliente3.descricaoCliente)
+console.log("---------------")
+console.log("---------------")
+console.log("Telefone: \n" + telefoneCelular3.descricaoTelefoneCelular)
+console.log("---------------")
+console.log("Endereço: \n" + endereco3.descricaoEndereco)
+
+console.log("---------------")
+
+
+console.log("\nOrdenação de clientes por nome.")
+
 // Objetivo 2
 
 let listaClientes = [cliente1, cliente2, cliente3]
 
-function OrdenaClientes(listaClientes){
-   return listaClientes.toSorted((a,b) => {
-      return a.nome.localeCompare(b.nome);
-   })
+console.log("-- Array de Nomes Ordenados--");
+
+function OrdenaApenasNomes(listaClientes) {
+   return listaClientes.toSorted((a, b) => a.nome.localeCompare(b.nome)).map(cliente => cliente.nome);                   
 }
 
-const resultadoApenasNomes = OrdenaClientes(listaClientes).map(c => c.nome);
+console.log(OrdenaApenasNomes(listaClientes));
 
-console.log("-- Array de Nomes Ordenados--");
-console.log(resultadoApenasNomes);
+console.log("=)")
